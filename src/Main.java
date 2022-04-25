e69f1c91eff7c5b858e2c27c809aa33126082594
unknown <703287771@genpact.com>
2022-04-25 11:24:23 +0530

    message

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    //for loop example
+
+ public static void main(string[] args)
